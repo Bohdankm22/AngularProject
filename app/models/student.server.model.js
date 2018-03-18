d71e56235f920870230bdc46efc676a3409cad9b
Bohdan <bohdansharipov@gmail.com>
2018-03-18 16:09:17 -0400

    Created model Student.

---
+++ app/models/student.server.model.js
@@ -0,0 +1,19 @@
+﻿// Load the Mongoose module and Schema object
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+// Define a new 'StudentSchema'
+const StudentSchema = new Schema({
+    studentNumber: Number,
+    password: String,
+    firstName: String,
+    lastName: String,
+    address: String,
+    city: String,
+    phoneNumber: String,
+    email: String,
+    program: String
+});
+
+// Create the 'Student' model out of the 'StudentSchema'
+mongoose.model('Student', StudentSchema);
