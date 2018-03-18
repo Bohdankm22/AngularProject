9718507967765d0a492a0f14271ef08bea7b0d15
Bohdan <bohdansharipov@gmail.com>
2018-03-18 16:09:09 -0400

    Created model Course.

---
+++ app/models/course.server.model.js
@@ -0,0 +1,14 @@
+﻿// Load the Mongoose module and Schema object
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+// Define a new 'StudentSchema'
+const CourseSchema = new Schema({
+    courseCode: String,
+    courseName: String,
+    section: String,
+    semester: Number
+});
+
+// Create the 'Course' model out of the 'CourseSchema'
+mongoose.model('Course', CourseSchema);
